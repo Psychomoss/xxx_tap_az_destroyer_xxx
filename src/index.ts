95f2e85f68f544779b2b4687b2b824d25f8bc094
Psychomoss <seyranunudov8@gmail.com>
2024-12-30 21:26:20 +0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// For more information, see https://crawlee.dev/
+import { PlaywrightCrawler, ProxyConfiguration } from 'crawlee';
+import { router } from './routes.js';
+import dotenv from "dotenv";
+
+dotenv.config();
+console.log(process.env.Id)
+const startUrls = ['https://crawlee.dev'];
+
+const crawler = new PlaywrightCrawler({
+    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    requestHandler: router,
+    // Comment this option to scrape the full website.
+    maxRequestsPerCrawl: 20,
+});
+
+await crawler.run(startUrls);
